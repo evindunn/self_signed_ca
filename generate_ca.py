563a93e0f0f8b068d7d79d1b27e42c6171ee9efd
Evin Dunn <evindunn14@gmail.com>
2022-10-16 11:31:53 -0700

    generate_ca cleanup

--- generate_ca.py
+++ generate_ca.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from datetime import timedelta
 from pathlib import Path
 from os.path import exists as path_exists
+from sys import exit as sys_exit
 
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
@@ -21,15 +22,14 @@ def main():
     argparser.add_argument("locality", help="The CA's locality")
     argparser.add_argument("organization", help="The name of the CA's organization")
     argparser.add_argument("email", help="The contact email for the CA")
-    argparser.add_argument("output_file", help="The output file for the certificate")
     argparser.add_argument("-k", "--private-key", help="The private key used to sign the certificate", default=None)
     args = argparser.parse_args()
 
     if args.private_key is not None:
         keyfile = args.private_key
         if not path_exists(keyfile):
-            print("Private key {} does not exist".format(keyfile))
-            exit(1)
+            print(f"Private key {keyfile} does not exist")
+            sys_exit(1)
    
         try:
             private_key_bytes = Path(args.private_key).read_bytes()
@@ -38,13 +38,13 @@ def main():
                 password=None
              )
         except Exception as e:
-            print("Invalid key file {}: {}".format(keyfile, e))
-            exit(1)
+            print(f"Invalid key file {keyfile}: {e}")
+            sys_exit(1)
 
-        print("Private key loaded from {}".format(keyfile))
+        print(f"Private key loaded from {keyfile}")
 
     else:
-        keyfile = "ca.key"
+        keyfile = f"{args.common_name}.key"
         private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         private_key_bytes = private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
@@ -52,9 +52,10 @@ def main():
             encryption_algorithm=serialization.NoEncryption()
         )
         private_key_path = Path(keyfile)
+        private_key_path.touch()
         private_key_path.chmod(0o600)
         private_key_path.write_bytes(private_key_bytes)
-        print("Private key written to {}".format(keyfile))
+        print(f"Private key written to {keyfile}")
 
     now = datetime.today()
     one_year_from_now = now + timedelta(days=365)
@@ -116,10 +117,11 @@ def main():
     cert = builder.sign(private_key=private_key, algorithm=hashes.SHA256())
     cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
 
-    output_path = Path(args.output_file)
+    output_crt_file = f"{args.common_name}.crt"
+    output_path = Path(output_crt_file)
     output_path.write_bytes(cert_pem)
 
-    print("Certificate written to {}".format(args.output_file))
+    print(f"Certificate written to {output_crt_file}")
 
 
 if __name__ == "__main__":
